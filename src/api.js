738d037731ec41d3bafb156f780cd0a989831236
serjus <serjus.dev@gmail.com>
2023-01-31 20:04:25 +0000

    last changes

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const searchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers:{
+            Authorization: 'Client-ID HLHFJw5bEs44qKQiq8zv7UsQ9BAo3q5j-SJT4lXMCZE'
+        },
+        params:{
+            query:term,
+        }
+    });
+
+    
+    return response.data.results
+}
+
+
+
+export default searchImages;
